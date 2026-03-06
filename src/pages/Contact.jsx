0be0bb19181e75a8039d5317701e392e17a54296
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-03-06 11:11:32 +0530

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import "../styles/contact.css";
+
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Contact Me</h1>
+
+            <p>Email: archidadwal@gmail.com</p>
+            <p>LinkedIn: https://www.linkedin.com/in/archi-dadhwal-886896292/</p>
+            <p>GitHub: https://github.com/archi-cell</p>
+        </div>
+    );
+}
+
+export default Contact;
